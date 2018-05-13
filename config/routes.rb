6979535f17d488f159da9e288041c59a6d524e56
jchlosta <j.chlosta@gazeta.pl>
2018-05-13 13:47:31 +0200

    Add like and unlike to article

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
   # delete 'articles/:id', to: 'articles#destroy'
 
   resources :articles do
-  resources :comments
+    resources :comments
+    resources :likes, only: %i[create destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
